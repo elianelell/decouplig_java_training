bfc0fcf4e02a86c885bef634632a0b43feb8c091
Eliane Lell <lelltchatchoua@et.esiea.fr>
2021-12-01 15:19:45 +0100

    Logger

---
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -0,0 +1,13 @@
+package fr.lernejo.logger;
+
+import java.util.function.Predicate;
+
+public class LoggerFactory {
+    public static Logger getLogger (String name)
+    {
+        Predicate<String> condition = message -> message.contains("simulation");
+        return new CompositeLogger(new ContextualLogger(name,new ConsoleLogger()) , new ContextualLogger(name,new FilteredLogger(new FileLogger("logs.txt"),condition)));
+    }
+
+
+}
